632aeadc7467c530a333a05e7f13e6bdfb081c72
Uri Berger <uri.berger2@mail.huji.ac.il>
2020-05-11 18:16:50 +0300

    Removed string multiplication from str_insertion

--- str_insertion.py
+++ str_insertion.py
@@ -4,11 +4,7 @@ my_char we insert a copy of str2, unless we exceeded the maximum number of inser
 Features tested:
 - Multiple if statements vs. complex boolean expression
 - Use of compound assignment operators vs. use of =
-- Use of "string multiplication" vs. a loop
 - Use of temporary variables vs. single line computation
-
-Possible bug creations:
-- "for _ in range(insert_count):" -> "for _ in range(cur_ind):"
 """
 
 
@@ -22,8 +18,7 @@ def long_str_insertion(str1, str2, my_char, max_insert_count):
             if insert_count < max_insert_count:
                 res = res + str1[prev_ind : cur_ind + 1]  # Use of =
                 insert_count = insert_count + 1  # Use of =
-                for _ in range(insert_count):  # Use a loop
-                    res = res + str2  # Use of =
+                res = res + str2  # Use of =
                 prev_ind = cur_ind + 1
     res = res + str1[prev_ind: cur_ind + 1]
 
@@ -38,7 +33,7 @@ def short_str_insertion(str1, str2, my_char, max_insert_count):
             str1[cur_ind] == my_char and insert_count < max_insert_count
         ):  # Complex boolean expression, single line computation
             insert_count += 1  # Use of a compound assignment operator
-            res += str1[prev_ind: cur_ind + 1] + insert_count * str2  # Use of a compound assignment operator
+            res += str1[prev_ind: cur_ind + 1] + str2  # Use of a compound assignment operator
             prev_ind = cur_ind + 1
     res += str1[prev_ind: cur_ind + 1]
     return res
